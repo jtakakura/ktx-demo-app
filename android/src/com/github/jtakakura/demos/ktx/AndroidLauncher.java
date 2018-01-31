f47a3efa574089cf88a7ee4c7b0f390e0c05a105
Junji Takakura <j.takakura@gmail.com>
2018-01-31 22:54:02 +0900

    initial commit

---
+++ android/src/com/github/jtakakura/demos/ktx/AndroidLauncher.java
@@ -0,0 +1,16 @@
+package com.github.jtakakura.demos.ktx;
+
+import android.os.Bundle;
+
+import com.badlogic.gdx.backends.android.AndroidApplication;
+import com.badlogic.gdx.backends.android.AndroidApplicationConfiguration;
+import com.github.jtakakura.demos.ktx.KtxDemoApp;
+
+public class AndroidLauncher extends AndroidApplication {
+	@Override
+	protected void onCreate (Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		AndroidApplicationConfiguration config = new AndroidApplicationConfiguration();
+		initialize(new KtxDemoApp(), config);
+	}
+}
